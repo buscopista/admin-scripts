7066264f7b99d999b106c5ed76fe9e71ec173744
cascara <cascara@prometeus.(none)>
2012-05-06 12:30:17 +0200

    adapted to the new log functions

--- files.sh
+++ files.sh
@@ -41,32 +41,32 @@ do_backup() {
     last_dir="$backups_dir/files/last"
     [[ -d $files_dir ]] \
     || {
-        log WARN "No source dir $files_dir found."
+        log::WARN "No source dir $files_dir found."
         return 1
     }
     [[ -d $last_dir ]] \
     || {
         mkdir -p $last_dir \
-        && log WARN "Last backup directory $last_dir not found, created a new one." \
+        && log::WARN "Last backup directory $last_dir not found, created a new one." \
         || {
-            log ERROR "Failed to create the backups dir $last_dir."
+            log::ERROR "Failed to create the backups dir $last_dir."
             return 1
         }
     }
     log "Rsyncing from original directory $files_dir"
     rsync --progress --stats -r -t -p -l --delete $files_dir $last_dir \
-    && log OK "Rsync Done." \
+    && log::OK "Rsync Done." \
     || {
-        log ERROR "Failed to rsync $files_dir to $last_dir. Aborting."
+        log::ERROR "Failed to rsync $files_dir to $last_dir. Aborting."
         return 1
     }
     name="$(basename $files_dir)"
     timestamp="$(date +%Y%m%d%H%M%S)"
     log "Creating tarfile $backups_dir/files/$name.$timestamp.tgz"
     tar cvzf $backups_dir/files/$name.$timestamp.tgz -C $last_dir/ $name  \
-    && log OK "Tar done." \
+    && log::OK "Tar done." \
     || {
-        log ERROR "Error creating tarfile $backups_dir/files/$name.$timestamp.tgz."
+        log::ERROR "Error creating tarfile $backups_dir/files/$name.$timestamp.tgz."
         return 1
     }
     return 0
@@ -78,32 +78,32 @@ restore()
     name="$(basename $files_dir)"
     [[ -d "$dir" ]] \
     || {
-        log WARN "No backup found at $dir. Nothing to restore."
+        log::WARN "No backup found at $dir. Nothing to restore."
         exit 1
     }
     log "Rsyncing $dir to $files_dir"
     rsync --progress --stats -r -t -p -l --delete $dir/$name/ $files_dir \
-    && log OK "Rsync Done." \
+    && log::OK "Rsync Done." \
     || {
-        log ERROR "Failed to rsync $files_dir to $dir. Aborting."
+        log::ERROR "Failed to rsync $files_dir to $dir. Aborting."
         return 1
     }
     file="$(ls -t $backups_dir/files/$name.*.tgz 2>/dev/null | head -n1)"
     log "Restoring old last dir from the newer tgz $file"
     [[ -f $file ]] \
     || {
-        log WARN "No tar backup found, skipping the restoration of the last directory." 
+        log::WARN "No tar backup found, skipping the restoration of the last directory." 
         return 0
     }
     rm -Rf $dir/$name \
     && {
         tar xvzf $file -C $dir \
-        && log OK "Last dir restored from $file." \
+        && log::OK "Last dir restored from $file." \
         || {
-            log ERROR "Error toring tarfile $file."
+            log::ERROR "Error toring tarfile $file."
             return 1
         }
-    } || log WARN "Unable to delete old last dir. Skipping restoration from tgz."
+    } || log::WARN "Unable to delete old last dir. Skipping restoration from tgz."
 }
 
 do_restore() {
@@ -135,7 +135,7 @@ do_update() {
                 ${svn_pass:+--password $svn_pass} \
                 --non-interactive $url $tmp_src_dir \
             || {
-                log ERROR "Error getting source code from $url to $tmp_src_dir."
+                log::ERROR "Error getting source code from $url to $tmp_src_dir."
                 return 1
             }
         }
@@ -143,14 +143,14 @@ do_update() {
     }
     [[ -d "$dir" ]] \
     || {
-        log WARN "No files found at $dir. Nothing to update."
+        log::WARN "No files found at $dir. Nothing to update."
         exit 1
     }
     log "Rsyncing $dir to $files_dir"
     rsync --progress --stats -r -t -p -l --exclude '.svn' --delete $dir/ $files_dir \
-    && log OK "Rsync Done." \
+    && log::OK "Rsync Done." \
     || {
-        log ERROR "Failed to rsync $files_dir to $dir. Aborting."
+        log::ERROR "Failed to rsync $files_dir to $dir. Aborting."
         return 1
     }
 }
@@ -169,10 +169,10 @@ shift $(($OPTIND - 1))
 OPTIND=1
 
 conf::load ${conf_file:?No conf_file specified} \
-|| { log ERROR "Error parsing config file"; exit 1; }
+|| { log::ERROR "Error parsing config file"; exit 1; }
 conf::require files_dir src_files_dir backups_dir tmp_src_dir \
 || {
-    log ERROR "Some required parameters are missing in the config file."
+    log::ERROR "Some required parameters are missing in the config file."
     exit 1
 }
 
